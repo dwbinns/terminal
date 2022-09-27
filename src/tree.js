b292691678364c06283c74618fdbad34810ffd3f
David Binns <dbinns.web+git@gmail.com>
2022-09-27 16:01:54 +0100

    Destructure tree options

--- src/tree.js
+++ src/tree.js
@@ -1,19 +1,36 @@
 import { single } from "@dwbinns/terminal/box";
 
-export default function tree(label, top, format) {
-    let {indent = 2, style = single} = format || {};
-    return `${label} ${top}`
-        + [...top.children]
-            .map(([key, value], index, list) => {
+export default function tree(
+    {
+        label = "",
+        node,
+        getDescription = x => x.toString(),
+        getChildren = x => x.children,
+        format,
+        path = []
+    }
+) {
+    let { indent = 2, style = single } = format || {};
+    let childPath = [...path, node];
+    return `${label} ${getDescription(node)}`
+        + getChildren(node, path)
+            .map(([childLabel, child], index, list) => {
                 let isLast = index < list.length - 1;
-                let hasChildren = !!value.children.size;
+                let hasChildren = !!getChildren(child, childPath).length;
                 let joiner = style(true, true, isLast, false).padEnd(indent, style(false, true, false, true))
                     + style(false, true, hasChildren, true)
                     + " ";
                 let margin = style(isLast, false, isLast, false).padEnd(indent);
                 return "\n"
                     + joiner
-                    + tree(key, value, format).replaceAll(/\n/g, `\n${margin}`)
+                    + tree({
+                        label: childLabel,
+                        node: child,
+                        getDescription,
+                        getChildren,
+                        format,
+                        path: childPath
+                    }).replaceAll(/\n/g, `\n${margin}`)
             })
             .join("");
 }
